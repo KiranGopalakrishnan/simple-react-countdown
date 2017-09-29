be7bf48491c1054ad20243455aefaee0f445fbb1
KiranGopalakrishnan <kirangplkrishnan@gmail.com>
2017-09-29 19:10:25 -0400

    Initial Commit

---
+++ src/index.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+
+class Countdown extends Component {
+  constructor(){
+    super();
+    this.state={
+      currentCount:true
+    };
+  }
+  componentDidMount(){
+    var countTo = parseInt(this.props.CountTo);
+    var time = parseInt(this.props.time);
+    //Time interval calculated based user input
+    var timerInterval = Math.round(countTo/time);
+    //Initial counter
+    var counter = 0;
+    //Checking if timerInterval is less than zero or not
+    var dividedByTimeResult = timerInterval<1?1:timerInterval;
+    //Pertaining the current scope
+    var selfe = this;
+    //Timer function
+    var tickTimer =setInterval(function(){
+      var requiredStep =(counter+parseInt(dividedByTimeResult*timerInterval))<countTo?parseInt((countTo/time)*timerInterval):parseInt(countTo-counter);
+      counter>=countTo?clearInterval(tickTimer):counter = counter+requiredStep;
+      if (selfe.refs.myRef) {
+        selfe.setState({currentCount:counter});
+      }
+    }, timerInterval);
+  }
+  render() {
+
+    return (
+      <span ref="myRef">{this.state.currentCount}</span>
+    );
+  }
+}
+
+export default Countdown;
